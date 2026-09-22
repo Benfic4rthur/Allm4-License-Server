c1fdd3e4319602b0d094414ce8938826da8e4d11
Arthur Benfica Graff <arthur_benfica@hotmail.com>
2026-09-22 19:45:13 -0300

    feat: register admin dashboard routes

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import { checkDatabaseConnection } from "./db.js";
 import licenseRouter from "./license-routes.js";
 import purchaseRouter from "./purchase-routes.js";
 import couponAdminRouter from "./coupon-admin-routes.js";
+import adminDashboardRouter from "./admin-dashboard-routes.js";
 import webhookRouter from "./webhook-routes.js";
 import bugRouter from "./bug-routes.js";
 import freeUsageRouter from "./free-usage-routes.js";
@@ -49,6 +50,7 @@ app.get("/api/health/db", async (_req, res) => {
 app.use("/api", licenseRouter);
 app.use("/api", purchaseRouter);
 app.use("/api", couponAdminRouter);
+app.use("/api", adminDashboardRouter);
 app.use("/api", webhookRouter);
 app.use("/api", bugRouter);
 app.use("/api", freeUsageRouter);
